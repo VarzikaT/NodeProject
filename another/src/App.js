ed311c5fe17e96359ea42e56601f7c2711e88f96
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-27 16:35:29 +0530

    uploading few basics

---
+++ another/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Documentation  from './Documentation';
+
+function App() {
+  return (
+    <div className="App">
+      <Documentation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ another/src/Documentation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Documentation() {
+    return (
+      <div>
+        <h1>This is our new component</h1>
+      </div>
+    );
+  }
