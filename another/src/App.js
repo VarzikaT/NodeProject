d4a55eb5b2c84626e15b3cac8ea9c56e5cccfa33
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-27 19:11:23 +0530

    simple counter text added

--- another/src/App.js
+++ another/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import Documentation  from './Documentation';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-      <Documentation/>
+     <header className="App-header">
+      <h2>Welcome to my counter :) </h2>
+      <Counter/>
+     </header>
     </div>
   );
 }
---
+++ another/src/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counter=()=>{
+    const count=0;
+    return(
+        <div>
+            <p>The counter is: {count}</p>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter;
